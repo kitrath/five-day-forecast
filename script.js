812242d9a3911068c144b81807c60b7ca107a40f
kitrath <crathmel@gmail.com>
2023-01-25 00:51:27 -0600

    add cache management, localStorage and buttons

--- script.js
+++ script.js
@@ -19,22 +19,31 @@ function buildCurrentWeatherURL(cityName) {
 }
 
 function buildFiveDayWeatherURL(coords) {
-    const lon = encodeURIComponent(coords.lat);
-    const lat = encodeURIComponent(coords.lon);
+    const lon = encodeURIComponent(coords.lon);
+    const lat = encodeURIComponent(coords.lat);
     let queryString = '?lat=' + lat + '&lon=' + lon;
     return completeURL(queryString, 'five');
 }
-// `type` arg can be one of 'current' or 'five'
-// TODO: Rewrite, because five-day forecast takes lat and lon instead of city name
-async function getWeatherDataForCity(cityName, type) {
-    let url = buildWeatherURLforCity(cityName, type);
+
+function buildURL(type, data) {
+    switch (type) {
+        case 'current':
+            return buildCurrentWeatherURL(data);
+        case 'five':
+            return buildFiveDayWeatherURL(data);
+        default:
+            throw new Error('Unreachable!');
+    }
+}
+
+async function getOpenWeatherData(type, data) {
+    let url = buildURL(type, data);
     try {
         const response = await fetch(url);
+        // response in 200s
         if (response.ok) {
-            // Without `await`, response.json() will not 
-            // throw if it fails. (The error would have
-            // to be caught with a .catch() handler by the 
-            // calling function.)
+            // Need `await` here so that, if response.json()
+            // rejects, our function can throw 
             return await response.json();
         } else {
             throw new Error(`Unexpected status code: ${
@@ -44,17 +53,23 @@ async function getWeatherDataForCity(cityName, type) {
     } catch (error) {
         console.error(error.message);
     }
-};
+}
 
-function createElem(tagName, ...classList) {
-    const el = document.createElement(tagName);
-    if (classList.length) {
-        document.setAttribute(
-            "class",
-            classList.join(" ")
-        );
+async function getCurrentWeatherJSON(cityName) {
+    try {
+        return await getOpenWeatherData('current', cityName);
+    } catch (error) {
+        // handleCityNotFounc(cityName);
+        console.error(error.message);
     }
-    return el;
+}
+
+async function getFiveDayForecastJSON(coords) {
+    try {
+        return await getOpenWeatherData('five', coords);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 // Extract only one forecast per day from a 5-day list of 3-hour forecasts
@@ -75,14 +90,189 @@ function getListOfFiveForecasts(dataList) {
     return resultList;
 }
 
-function getDate(dateObj) {
-    // If no "," is present, entire locale string at index 0
-    return dateObj.toLocaleString().split(",")[0];
+const citySearchButton = document.querySelector("#city-search-btn");
+const citySearchInput = document.querySelector("#city-search");
+
+citySearchButton.addEventListener('click', function(event) {
+    // Don't refresh page
+    event.preventDefault();
+
+    const city = citySearchInput.value.trim();
+    
+    // city is ''
+    if (!city) {
+        return;
+    }
+
+    // Check cache
+    const cachedCity = JSON.parse(localStorage.getItem(city))
+
+    if (cachedCity && isCachedForecastValid(cachedCity.timestamp)) {
+        loadWeatherDataFromCache(city);
+        // exit 
+        return;
+    }
+
+    getCurrentWeatherJSON(city)
+
+        .then(function (data) {
+
+            console.log(data);
+            // TODO: displayCurrentForecast();
+
+            // cache in localStorage
+            addCityToLocalStorage(city, data);
+
+            return data.coord;
+        })
+        // coords is {lat: number, lon: number}
+        .then(function(coords) {
+            return getFiveDayForecastJSON(coords);
+        })
+        
+        // fiveDayJSON is large list of 5 days worth of 3-hour forecasts 
+        .then(function(fiveDayJSON) {
+            // We pick one forecast (at noon) for each day
+            const fiveDayAbbr = getListOfFiveForecasts(fiveDayJSON.list);
+            // TODO: displayFiveDayForecast()
+
+            // add to localStorage cache
+            addFiveDayToLocalStorage(city, fiveDayAbbr);
+
+            console.log(fiveDayAbbr);
+        })
+
+        .catch(function(error) {
+            console.error(error.message);
+        })
+});
+
+/*************** localStorage cache management ***********************/
+
+function addCityToLocalStorage(city, data) {
+
+    addToCachedCitiesList(city);
+    createCachedButton(city);
+
+    localStorage.setItem(
+        city,
+        JSON.stringify({
+            timestamp: Date.now(),
+            current: data
+        })
+    );
+}
+
+function addFiveDayToLocalStorage(city, data) {
+    let cachedForecast = JSON.parse(localStorage.getItem(city));
+    cachedForecast.fiveday = data;
+    localStorage.setItem(city, JSON.stringify(cachedForecast));
+}
+
+function loadWeatherDataFromCache(city) {
+    
 }
 
 function isCachedForecastValid(timeStamp, numHoursToCache = 3) {
     // 3,600,000 milliseconds in an hour
     const timeLimit = numHoursToCache * 3600000;
-    const now = new Date.now();
+    const now = Date.now();
     return (now - timeStamp) < timeLimit;
+}
+
+// Might return []
+// Removes invalid entries from localStorage and 
+// from the 'cities' list
+// Removes invalid buttons from DOM
+// Caller responsible for stringify and store
+function getValidCitiesList() {
+    let cities = JSON.parse(localStorage.getItem("cities"));
+
+    if (!cities) return;
+
+    for (city of cities) {
+        const cachedForecast = JSON.parse(localStorage.getItem(city));
+        if (cachedForecast && isCachedForecastValid(cachedForecast.timestamp)) {
+            // great. move on.
+            continue;
+        } else {
+            removeCachedButton(city);
+            localStorage.removeItem(city);
+            // Don't alter list in loop
+            city = null;
+        }
+    }
+    return cities.filter(x => x);
+}
+
+// Adds city name to list of cached cities.
+// If list doesn't exist, create one, add item
+// and save to localStorage
+function addToCachedCitiesList(city) {
+    let cities = getValidCitiesList(); 
+    if (cities) {
+        cities.push(city);
+    } else {
+        // cities === undefined
+        cities = [city];
+    }
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+// (on page load) determines if cached forecasts are still
+// valid and creates a button for the valid ones
+function pruneCachedForecasts(event) {
+
+    const updatedCities = getValidCitiesList();
+
+    if (updatedCities && updatedCities.length) {
+        // create button for each survivor
+        for (city of updatedCities) {
+            createCachedButton(city);
+        }
+        localStorage.setItem(
+            "cities", 
+            JSON.stringify(updatedCities)
+        );
+    } else {
+        localStorage.removeItem("cities");
+    }
+}
+
+document.addEventListener('DOMContentLoaded', pruneCachedForecasts); 
+
+function createCachedButton(cityName) {
+
+    const container = document.querySelector('#cities');
+    const button = createElem(
+        'button', 'cached', 'btn', 'btn-outline-secondary', 'btn-sm'
+    );
+    button.setAttribute('data-city', cityName);
+    button.textContent = cityName;
+    container.appendChild(button);
+}
+
+function removeCachedButton(cityName) {
+    const container = document.querySelector('#cities');
+    const selector = `[data-city="${cityName}"]`;
+    const button = document.querySelector(selector);
+    if (button) {
+        container.removeChild(button);
+    }
+}
+
+function createElem(tagName, ...classList) {
+    const el = document.createElement(tagName);
+    if (classList.length) {
+        el.setAttribute(
+            "class",
+            classList.join(" ")
+        );
+    }
+    return el;
+}
+
+function getDate(dateObj) {
+    // If no "," is present, entire locale string at index 0
+    return dateObj.toLocaleString().split(",")[0];
 }
